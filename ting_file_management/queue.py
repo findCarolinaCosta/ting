3df0ad583b83d2fd83b2ed12e2bf5c0cf22cfce6
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-08-14 17:59:47 -0300

    Implement a queue to store files that will be read

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,21 @@
+from collections import deque
+
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.data = deque()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.data)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        return self.data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        return self.data.popleft()
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if (0 <= index < len(self)):
+            return self.data[index]
+
+        raise IndexError
+
