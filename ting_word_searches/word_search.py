e5d344ec497fafa14c7d52f7143c9931347ca296
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-08-14 19:49:17 -0300

    Implement `exists_word` function

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,25 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+
+    for index in range(len(instance)):
+        file = instance.search(index)
+
+        data = {
+            "palavra": word,
+            "arquivo": file["nome_do_arquivo"],
+            "ocorrencias": [
+                {"linha": index + 1}
+                for index, line in enumerate(file["linhas_do_arquivo"])
+                if word in line.lower()
+            ],
+        }
+
+        if data["ocorrencias"]:
+            result.append(data)
+        else:
+            []
+
+    return result
 
 
 def search_by_word(word, instance):
